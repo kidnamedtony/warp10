1e66c8414c8533c03a80bb51bcebeb0db3d58676
Tony Loiseleur <a.loiseleur@gmail.com>
2019-11-13 12:47:23 -0800

    Fix many small bugs in clustering script ahead of hybridization.

--- src/ep_clustering.py
+++ src/ep_clustering.py
@@ -32,7 +32,7 @@ def clusterize(opt_clusters=13, rand_state=8, imdb_rating_threshhold=7.0, max_fe
     combined_master_df = create_df_for_vectorization()
 
     # Following on from the previous line, here is where we call the vectorize function on the DF:
-    X, features_arr = vectorize(max_features=max_feat)
+    X, features_arr = vectorize(combined_master_df, max_features=max_feat)
 
     # Here we fit the vectorized data to our k-Means algorithm:
     kmeans_clusterer = KMeans(n_clusters=opt_clusters,
@@ -42,12 +42,15 @@ def clusterize(opt_clusters=13, rand_state=8, imdb_rating_threshhold=7.0, max_fe
                               )
     kmeans_clusterer.fit(X)
 
+    # Here, we create our recommendation DF so that we can append recs to it:
+    kmeans_recommendation_df = pd.DataFrame(columns=["series", "season", "episode", "ep_title", "identifier", "IMDB_user_rating"])
+
     # After the data is fit to the vectorizer, we can determine our cluster centers and their top 15 terms:
     centroid_array = kmeans_clusterer.cluster_centers_
     top_centroids = centroid_array.argsort()[:, -1:-16:-1]
     print("Top 15 words for each cluster:\n")
     for num, centroid in enumerate(top_centroids):
-        print(f"Centroid {num} :",", ".join(features[i] for i in centroid),"\n")
+        print(f"Centroid {num} :",", ".join(features_arr[i] for i in centroid),"\n")
 
     # From here to the end of the function, we print out one not-so-random episode for each cluster:
     print(f"A random GOOD ({imdb_rating_threshhold}+ rating) episode in each cluster:\n")
@@ -56,16 +59,74 @@ def clusterize(opt_clusters=13, rand_state=8, imdb_rating_threshhold=7.0, max_fe
     # These next 3 lines give us an episode rec proposal. If it passes the ratings screen in the next block of code, then it graduates from proposal to an actual recommendation:
     for idx in range(kmeans_clusterer.n_clusters):
         cluster = np.arange(0, X.shape[0])[assigned_cluster==idx]
-        ep_title_proposal = np.random.choice(cluster, 1, replace=False)
+        sample_ep_titles = np.random.choice(cluster, 1, replace=False)
 
-        # This is where we filter out episodes below a certain IMDb ratings thresshold (as set by the parameter up top):
-        if combined_master_df.loc[ep_title_proposal, "IMDB_user_rating"].values >= imdb_rating_threshhold:
-            sample_ep_titles = ep_title_proposal
-        else:
-            continue
+        # This is where we filter out episodes below a certain IMDb ratings thresshold (as set by the parameter up top). The count variable is to guarantee that that we don't search forever in a cluster for an episode with a really high rating if (for example) no episodes in a given cluster has a rating that high:
+        count = 0
+        while (combined_master_df.loc[sample_ep_titles, "IMDB_user_rating"].values < imdb_rating_threshhold) and (count < (len(assigned_cluster) // 2) + 2):
+            sample_ep_titles = np.random.choice(cluster, 1, replace=False)
+            count += 1
+            if combined_master_df.loc[sample_ep_titles, "IMDB_user_rating"].values >= imdb_rating_threshhold:
+                continue
 
         # Prints cluster number prior to printing the random episode recommendation from that cluster:
-        print("Cluster %d:" % i)
+        print("Cluster %d:" % idx)
+
+        # Some text-prettying before printing the episode recommendation AND appending it to a recommendation DF:
+        for ep_title in sample_ep_titles:
+            if combined_master_df.loc[ep_title]["episode"] <= 9:
+                episode = f'0{combined_master_df.loc[ep_title]["episode"]}'
+            else:
+                episode = f'{combined_master_df.loc[ep_title]["episode"]}'
+            series = f'{combined_master_df.loc[ep_title]["series"]}'
+            season = f'{combined_master_df.loc[ep_title]["season"]}'
+            title = f'{combined_master_df.loc[ep_title]["ep_title"]}'
+            ident = f'{combined_master_df.loc[ep_title]["identifier"]}'
+            rating = f'{combined_master_df.loc[ep_title]["IMDB_user_rating"]}'
+            print(f'{series} S:0{season} E:{episode} -- {title} -- {rating}')
+
+            # This is where that aforementioned recommendation DF gets built:
+            rec_row_list = [[series, int(season), int(episode), title, ident, float(rating)]]
+            single_rec_df = pd.DataFrame(rec_row_list, columns=["series",
+                                                                "season",
+                                                                "episode",
+                                                                "ep_title",
+                                                                "identifier",
+                                                                "IMDB_user_rating"])
+            kmeans_recommendation_df = pd.concat([kmeans_recommendation_df, single_rec_df])
+
+    return assigned_cluster, combined_master_df, kmeans_recommendation_df
+
+
+
+def get_new_recs_from_existing_clusters(assigned_cluster, combined_master_df, imdb_rating_threshhold=7.0):
+    """
+    Function outputs new recommendations without having to rerun the clusterize function (which can be time-consuming).
+    """
+
+    # This is basically the same lines of code at the end of the clusterize function above. So long as you pass in the assigned_cluster variable and the combined_master_df as arguments, this function should be able to provide new recommendations:
+    combined_master_df = create_df_for_vectorization()
+
+    # Here, we create our recommendation DF so that we can append recs to it:
+    kmeans_recommendation_df = pd.DataFrame(columns=["series", "season", "episode", "ep_title", "identifier", "IMDB_user_rating"])
+
+    print(f"A random GOOD ({imdb_rating_threshhold}+ rating) episode in each cluster:\n")
+
+    # These next 3 lines give us an episode rec proposal. If it passes the ratings screen in the next block of code, then it graduates from proposal to an actual recommendation:
+    for idx in set(assigned_cluster):
+        cluster = np.arange(0, assigned_cluster.shape[0])[assigned_cluster==idx]
+        sample_ep_titles = np.random.choice(cluster, 1, replace=False)
+
+        # This is where we filter out episodes below a certain IMDb ratings thresshold (as set by the parameter up top). The count variable is to guarantee that that we don't search forever in a cluster for an episode with a really high rating if (for example) no episodes in a given cluster has a rating that high:
+        count = 0
+        while (combined_master_df.loc[sample_ep_titles, "IMDB_user_rating"].values < imdb_rating_threshhold) and (count < (len(assigned_cluster) // 2) + 2):
+            sample_ep_titles = np.random.choice(cluster, 1, replace=False)
+            count += 1
+            if combined_master_df.loc[sample_ep_titles, "IMDB_user_rating"].values >= imdb_rating_threshhold:
+                continue
+
+        # Prints cluster number prior to printing the random episode recommendation from that cluster:
+        print("Cluster %d:" % idx)
 
         # Some text-prettying before printing the episode recommendation AND appending it to a recommendation DF:
         for ep_title in sample_ep_titles:
@@ -92,6 +153,8 @@ def clusterize(opt_clusters=13, rand_state=8, imdb_rating_threshhold=7.0, max_fe
 
     return kmeans_recommendation_df
 
+
+
 def create_df_for_vectorization():
     """
     Function to create dataframe for vectorization. Returns a dataframe for that task.
@@ -107,7 +170,7 @@ def create_df_for_vectorization():
 
 
 
-def vectorize(max_features=None):
+def vectorize(combined_master_df, max_features=None):
     """
     Function to vectorize the word blobs in our DF. Returns X and features arrays.
     """
@@ -123,7 +186,7 @@ def vectorize(max_features=None):
     # Setting up our vectorization method, which takes in the above variables:
     vectorizer = TfidfVectorizer(strip_accents="ascii",
                                  lowercase=True,
-                                 tokenizer=test_tokenizer.tokenize,
+                                 tokenizer=tokenizer.tokenize,
                                  stop_words=stopwords_,
                                  max_features=None
                                  )
@@ -132,8 +195,6 @@ def vectorize(max_features=None):
     X = vectorizer.fit_transform(combined_master_df["ultra_blob"])
 
     features = vectorizer.get_feature_names()
-    print(f"Shape of X: {not_X.shape}")
-    print(f"X as dense matrix: {not_X.todense()}")
     return X, features
 
 
